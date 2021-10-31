c040482632bdf03e7ff6d0e5a7ecb17afeafffaa
Jason Ganub <jasonganub@gmail.com>
2021-10-31 20:13:05 +0700

    Add init-db command

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 run:
 	FLASK_ENV=development FLASK_APP=flaskr flask run
+
+init-db:
+	FLASK_APP=flaskr flask init-db
--- flaskr/db.py
+++ flaskr/db.py
@@ -5,12 +5,16 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
 
 @click.command('init-db')
 @with_appcontext
