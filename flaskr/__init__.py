409f50fb0d4eaedaa8e874930ae4ff7c5cef74a0
Jason Ganub <jasonganub@gmail.com>
2021-11-05 16:54:27 +0700

    Add auth file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-from . import db
+from . import (db, auth)
 
 
 def create_app(test_config=None):
@@ -30,6 +30,7 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
 
+    app.register_blueprint(auth.bp)
     db.init_app(app)
 
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -5,7 +5,6 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
-
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
