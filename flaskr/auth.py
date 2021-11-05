b6812a6e2a0abd35f2d87cae79cf5d7d9c0af27a
Jason Ganub <jasonganub@gmail.com>
2021-11-05 18:02:06 +0700

    Add login route and function

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -33,3 +33,33 @@ def register():
 				error = f"User {username} is already registered"
 			else:
 				return redirect(url_for("auth.login"))
+		flash(error)
+
+	return render_template('auth/register.html')
+
+bp.route('/login', methods=('GET', 'POST'))
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		db = get_db()
+		error = None
+
+		user = db.execute(
+			"SELECT * FROM user where (username) VALUES (?)",
+			(username)
+		).fetchone()
+
+		if user is None:
+			error = 'incorrect username'
+		elif generate_password_hash(password) != user['password']:
+			error = 'incorrect password'
+
+		if error is None:
+			session.clear()
+			session['user_id'] = user['id']
+			return redirect(url_for('index'))
+
+		flash(error)
+
+	return render_template('auth/login.html')
